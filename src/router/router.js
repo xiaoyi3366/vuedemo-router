99a5e1b36acbde0a0a1cffbe122ced29290d6d76
xiaoyi3366 <1326245718@qq.com>
2019-05-09 14:09:04 +0800

    导航

--- src/router/router.js
+++ src/router/router.js
@@ -4,19 +4,20 @@ Vue.use(VueRouter)
 
 import Main from '../components/Main.vue'
 const routes = [
-    {
-        path:'/daohang1',
-        component:Main
-    },
-    {
-        path:'/daohang2',
-        component:Main
-    },
-    {
-        path:'/daohang3',
-        component:Main
-    },
-    {path: '*',redirect:'/daohang1'}
+    // {
+    //     path:'/daohang1',
+    //     component:Main
+    // },
+    // {
+    //     path:'/daohang2',
+    //     component:Main
+    // },
+    // {
+    //     path:'/daohang3',
+    //     component:Main
+    // },
+    // {path: '*',redirect:'/daohang1'}
+    {path: '*',component:Main}
 ]
 const router = new VueRouter({
     routes
